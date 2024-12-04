d11c89e7723119c3e6bd1b964eb72d002867aec4
sounmu <hyginus011111@gmail.com>
2024-12-04 21:42:00 +0900

    refactor: Clean up imports and remove unused email configuration in main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
-import random
-import string
+from fastapi_mail import ConnectionConfig
 
 from config import Settings
-from routes.user_route import router as user_router
 from routes.auth_route import router as auth_router
+from routes.user_route import router as user_router
+
 settings = Settings()
 
 
@@ -29,17 +28,6 @@ origins = [
     "https://localhost:8000"
 ]
 
-conf = ConnectionConfig(
-    MAIL_USERNAME="official@healthcommu.com",
-    MAIL_PASSWORD="umm",
-    MAIL_FROM="official@healthcommu.com",
-    MAIL_PORT=587,
-    MAIL_SERVER="smtp.gmail.com",
-    MAIL_TLS=True,
-    MAIL_SSL=False,
-    USE_CREDENTIALS=True,
-    VALIDATE_CERTS=True
-)
 
 app.add_middleware(
     CORSMiddleware,
